89d7134a809f7967b9b0cc912087aa41a97b2681
Hevelise - Liz <liz.souza.dias@gmail.com>
2024-05-03 16:13:15 -0300

    feat: inclusão de URLs de imagens.

--- app_movies/lib/pages/widgets/lista_populares.dart
+++ app_movies/lib/pages/widgets/lista_populares.dart
@@ -2,7 +2,6 @@ import 'package:app_movies/pages/Sinopse/sinopse.dart';
 import 'package:flutter/material.dart';
 import 'package:app_movies/core/app_images.dart';
 
-
 class ListaFilmes extends StatelessWidget {
   const ListaFilmes({Key? key}) : super(key: key);
 
@@ -11,17 +10,17 @@ class ListaFilmes extends StatelessWidget {
       onTap: () {
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Sinopse()), 
+          MaterialPageRoute(builder: (context) => Sinopse()),
         );
       },
       child: SizedBox(
         width: 150,
-        height: 140,
+        height: 130,
         child: ListTile(
-          contentPadding: EdgeInsets.zero,
+          contentPadding: EdgeInsets.fromLTRB(15, 0, 0, 0,),
           title: ClipRRect(
             borderRadius: BorderRadius.circular(8),
-            child: Image.asset(
+            child: Image.network(
               imagePath,
               fit: BoxFit.cover,
             ),
@@ -39,14 +38,14 @@ class ListaFilmes extends StatelessWidget {
         children: <Widget>[
           padraoImage(context, AppImages.filme1),
           padraoImage(context, AppImages.filme2),
-          padraoImage(context, AppImages.filme1),
-          padraoImage(context, AppImages.filme2),
-          padraoImage(context, AppImages.filme1),
-          padraoImage(context, AppImages.filme1),
-          padraoImage(context, AppImages.filme2),
-          padraoImage(context, AppImages.filme1),
-          padraoImage(context, AppImages.filme2),
-          padraoImage(context, AppImages.filme1),
+          padraoImage(context, AppImages.filme3),
+          padraoImage(context, AppImages.filme4),
+          padraoImage(context, AppImages.filme5),
+          padraoImage(context, AppImages.filme6),
+          padraoImage(context, AppImages.filme7),
+          padraoImage(context, AppImages.filme8),
+          padraoImage(context, AppImages.filme9),
+          padraoImage(context, AppImages.filme10),
         ],
       ),
     );
