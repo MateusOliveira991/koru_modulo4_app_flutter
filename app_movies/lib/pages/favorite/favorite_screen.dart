9cc7c90f2fdf93fdab134a3b7e2c6d506ebdcbe6
Thalyta Diniz <thalytadiniz03@gmail.com>
2024-05-22 21:33:31 -0300

    feat: provider e inicio de gerenciamento de estados

---
+++ app_movies/lib/pages/favorite/favorite_screen.dart
@@ -0,0 +1,40 @@
+// favorite_screen.dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:app_movies/providers/favorites_provider.dart';
+import 'package:app_movies/models/movie.dart';
+import 'package:app_movies/pages/Home/home_details_screen.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final favoriteMovies = context.watch<FavoritesProvider>().favoriteMovies;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favorites'),
+      ),
+      body: ListView.builder(
+        itemCount: favoriteMovies.length,
+        itemBuilder: (context, index) {
+          final movie = favoriteMovies[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => MovieDetailScreen(movie: movie),
+                ),
+              );
+            },
+            child: ListTile(
+              leading: Image.network(movie.image),
+              title: Text(movie.title),
+              subtitle: Text(movie.originalTitle),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ app_movies/lib/providers/favorites_provider.dart
@@ -0,0 +1,22 @@
+// favorites_provider.dart
+import 'package:flutter/material.dart';
+import 'package:app_movies/models/movie.dart';
+
+class FavoritesProvider with ChangeNotifier {
+  final List<Movie> _favoriteMovies = [];
+
+  List<Movie> get favoriteMovies => _favoriteMovies;
+
+  void toggleFavorite(Movie movie) {
+    if (_favoriteMovies.contains(movie)) {
+      _favoriteMovies.remove(movie);
+    } else {
+      _favoriteMovies.add(movie);
+    }
+    notifyListeners();
+  }
+
+  bool isFavorite(String id) {
+    return _favoriteMovies.any((movie) => movie.id == id);
+  }
+}
