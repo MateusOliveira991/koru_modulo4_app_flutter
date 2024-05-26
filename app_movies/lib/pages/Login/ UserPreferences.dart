bedac96aabd1429f22e4de66a714b86a6c068d42
marcotuliosr <50932692+marcotuliosr@users.noreply.github.com>
2024-05-25 21:57:53 -0300

    feat: Gerenciando salvando e validando o usuario no login

---
+++ app_movies/lib/pages/Login/ UserPreferences.dart
@@ -0,0 +1,18 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserPreferences {
+  Future<void> saveUser(String email, String password) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(email, password);
+  }
+
+  Future<String?> getUserPassword(String email) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(email);
+  }
+
+  Future<bool> userExists(String email) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.containsKey(email);
+  }
+}
--- app_movies/lib/pages/Login/login_page.dart
+++ app_movies/lib/pages/Login/login_page.dart
@@ -1,75 +1,66 @@
-import 'package:app_movies/core/app_images.dart';
+import 'package:app_movies/pages/Login/%20UserPreferences.dart';
 import 'package:flutter/material.dart';
-import 'package:app_movies/pages/Home/home_page.dart';
 import 'package:flutter_login/flutter_login.dart';
+import 'package:app_movies/core/app_images.dart';
 import 'package:app_movies/core/app_colors.dart';
-
-
-
-
-
-const users =  {
-  'mateus@gmail.com': '12345',
-  'teste@gmail.com': '12345',
-  
-
-};
+import 'package:app_movies/pages/Home/home_page.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({super.key});
 
   Duration get loginTime => const Duration(milliseconds: 2250);
 
-  Future<String?> _authUser(LoginData data) {
+  UserPreferences get _userPreferences => UserPreferences();
+
+  Future<String?> _authUser(LoginData data) async {
     debugPrint('Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'User not exists';
-      }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
-      }
-      return null;
-    });
+    await Future.delayed(loginTime);
+    final storedPassword = await _userPreferences.getUserPassword(data.name);
+    if (storedPassword == null) {
+      return 'Usuário não existe';
+    }
+    if (storedPassword != data.password) {
+      return 'A senha informada não confere';
+    }
+    return null;
   }
 
-  Future<String?> _signupUser(SignupData data) {
+  Future<String?> _signupUser(SignupData data) async {
     debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      return null;
-    });
+    await Future.delayed(loginTime);
+    final userExists = await _userPreferences.userExists(data.name!);
+    if (userExists) {
+      return 'Usuário já existe';
+    }
+    await _userPreferences.saveUser(data.name!, data.password!);
+    return null;
   }
 
-  Future<String> _recoverPassword(String name) {
-  debugPrint('Name: $name');
-  return Future.delayed(loginTime).then((_) {
-    if (!users.containsKey(name)) {
-      return 'User not exists';
+  Future<String> _recoverPassword(String name) async {
+    debugPrint('Name: $name');
+    await Future.delayed(loginTime);
+    final userExists = await _userPreferences.userExists(name);
+    if (!userExists) {
+      return 'Usuário não existe';
     }
     return '';
-  });
-}
+  }
 
   @override
-Widget build(BuildContext context) {
-  return FlutterLogin(
-    logo: AppImages.login,
-   
-    onLogin: _authUser,
-    onSignup: _signupUser,
-    onSubmitAnimationCompleted: () {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (context) => const MyHomePage(),
-      ));
-    },
-    onRecoverPassword: _recoverPassword,
-    theme: LoginTheme(
-      primaryColor: AppColors.backgroundColor,
-      
-       
-
-      
-    ),
-  );
-}
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      logo: AppImages.login,
+      onLogin: _authUser,
+      onSignup: _signupUser,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => const MyHomePage(),
+        ));
+      },
+      onRecoverPassword: _recoverPassword,
+      theme: LoginTheme(
+        primaryColor: AppColors.backgroundColor,
+      ),
+    );
+  }
 }
