1dc32d7aa41cb82ec1e71b47c2b85be4f03ad717
Thalyta Diniz <thalytadiniz03@gmail.com>
2024-06-12 12:52:36 -0300

    test: renderização da home tabs

---
+++ app_movies/test/pages/Home/home_tabs_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:app_movies/pages/home_tabs.dart';
+
+void main() {
+  group('HomeTabs', () {
+    testWidgets('renderização da home tabs', (WidgetTester tester) async {
+
+      await tester.pumpWidget(MaterialApp(home: HomeTabs()));
+
+      expect(find.byType(HomeTabs), findsOneWidget);
+
+      expect(find.byType(TabBarView), findsOneWidget);
+
+      expect(find.byType(BottomNavigationBar), findsOneWidget);
+
+      expect(find.text('Home'), findsOneWidget);
+      expect(find.text('Favorites'), findsOneWidget);
+      expect(find.text('Books'), findsOneWidget);
+      expect(find.byIcon(Iconsax.home), findsOneWidget);
+      expect(find.byIcon(Iconsax.ticket), findsOneWidget);
+      expect(find.byIcon(Iconsax.book_1), findsOneWidget);
+    });
+  });
+}
